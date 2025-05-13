5beac73e4fea5f0bf08b0561d909b1c8b1284680
Himantha Thathsara <himanthathathsara2005@gmail.com>
2025-05-13 12:17:41 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,47 @@
+
 import { Statistics } from "./Statistics";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from '@gsap/react';
+import ScrollTrigger from "gsap/ScrollTrigger";
+
 import pilot from "../assets/pilot.png";
 
 export const About = () => {
+  gsap.registerPlugin(ScrollTrigger , useGSAP);
+
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    const element = textRef.current;
+    gsap.fromTo(
+      element,
+      { 
+        opacity: 0, 
+        y: 50 
+      },
+      {
+        opacity: 1,
+        y: 0,
+        scale: 0.95,
+        duration: 0.8,
+        delay: 1.8,
+        ease: 'power2.in',
+          scrollTrigger: {
+            trigger: element,
+            start: "top 70%",
+            end: "top 0%",
+            scrub: true,
+        },
+      }
+    );
+  }, []);
+
   return (
     <section
       id="about"
       className="container py-24 sm:py-32"
+      ref={textRef}
     >
       <div className="bg-muted/50 border rounded-lg py-12">
         <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
@@ -22,7 +58,7 @@ export const About = () => {
                 </span>
                 Company
               </h2>
-              <p className="text-xl text-muted-foreground mt-4">
+              <p className="text-xl text-muted-foreground mt-4" >
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
